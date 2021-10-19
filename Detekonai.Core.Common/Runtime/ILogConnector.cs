2da642af30444af38061c607140778e8008c8c54
aeai <aeai@detekonai.com>
2021-10-19 18:58:40 +0200

    More sane way to abstract logs

---
+++ Detekonai.Core.Common/Runtime/ILogConnector.cs
@@ -0,0 +1,17 @@
+﻿using System.Runtime.CompilerServices;
+namespace Detekonai.Core.Common
+{
+	public interface ILogConnector
+	{
+		public enum LogLevel
+		{
+			Verbose,
+			Info,
+			Warning,
+			Error,
+		}
+
+		public void Log(object sender, string msg,  LogLevel level = LogLevel.Verbose, [CallerMemberName] string memberName = "", [CallerFilePath] string sourceFilePath = "", [CallerLineNumber] int sourceLineNumber = 0);
+	}
+
+}
